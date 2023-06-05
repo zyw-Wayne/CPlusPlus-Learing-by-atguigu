6f80065b62529c3936195beb1bb0f241a7b5dde1
羅正 <55725764+zyw-Wayne@users.noreply.github.com>
2023-06-05 17:51:39 +0800

    add:尚硅谷文档； add:sample-love heart

---
+++ sample/love_heart.cpp
@@ -0,0 +1,40 @@
+/*
+ * @Author: zyw_Wayne
+ * @Date: 2023-06-05 17:37:26
+ * @LastEditTime: 2023-06-05 17:49:39
+ */
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+
+  // love heart function:(x^2+y^2-a)^3 - x^2 y^3 = 0
+  int a = 1.25;
+  // define bound
+  double bound = 1.3 * sqrt(a);
+
+  // x,y change step
+  double step = 0.05;
+
+  for (double y = bound; y >= -bound; y -= step)
+  {
+    for (double x = -bound; x <= bound; x += step / 2)
+    {
+      double result = pow((pow(x, 2) + pow(y, 2) - a), 3) - pow(x, 2) * pow(y, 3);
+      if (result <= 0)
+      {
+        cout << "*";
+      }
+      else
+      {
+        cout << " ";
+      }
+    }
+    cout << endl;
+  }
+  cin.get();
+
+  return 0;
+}
