a63de04f2a632a46fa8ccd9ccc65c74651df00dc
羅正 <55725764+zyw-Wayne@users.noreply.github.com>
2023-06-05 16:35:36 +0800

    sample-判断质数

--- sample/prime_number.cpp
+++ sample/prime_number.cpp
@@ -1,11 +1,12 @@
 /*
  * @Author: zyw_Wayne
  * @Date: 2023-06-05 10:41:52
- * @LastEditTime: 2023-06-05 10:52:54
+ * @LastEditTime: 2023-06-05 16:32:39
  */
 #include <iostream>
 using namespace std;
 
+// 怎样优雅地判断一个数是不是质数？ https://zhuanlan.zhihu.com/p/113815619
 bool isPrime(int num)
 {
   if (num <= 1)
@@ -14,4 +15,29 @@ bool isPrime(int num)
     if (num % i == 0)
       return false;
   return true;
+}
+
+int main()
+{
+
+  int num;
+  cout << "please input some number(between 0 and 2000000000) you want to check" << endl;
+  cin >> num;
+
+  if (isPrime(num))
+    cout << "this number is prime number" << endl;
+  else
+    cout << "this number is not prime number" << endl;
+
+  cout << "show The sum of all primes between 0 and 100:" << endl;
+
+  for (int i = 2; i <= 100; i++)
+  {
+    if (isPrime(i))
+      cout << i << "\t";
+  };
+
+  cin.get();
+  cin.get();
+  return 0;
 }
